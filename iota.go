dc8810ae037e52891d7a12d5099a59572c1cc457
DateKyosuke <rakoot@yandex.ru>
2022-09-27 21:09:36 +0300

    hello go

--- iota.go
+++ iota.go
@@ -4,17 +4,24 @@ import (
 	"fmt"
 )
 
-const (
-	a = iota + 1
-	_
-	b
-	c
-	d = c + 2
-	t
-	i
-	i2 = iota + 2
-)
-
 func main() {
-	fmt.Println(i2)
+	var c float32 = 9
+	switch {
+	case 1 <= c && c <= 9:
+		fmt.Print("от 1 до 9 ")
+		c--
+		fallthrough
+	case c == 10.2:
+		fmt.Print("пройден ")
+		fmt.Print(c)
+	case 100 <= c && c <= 250:
+		fmt.Print("от 100 до 250 ")
+		fmt.Print(c)
+	case 1000 <= c && c <= 6000:
+		c += 999
+		fmt.Print("от 1000 до 6000 ")
+		fallthrough
+	default:
+		fmt.Print("default ")
+	}
 }
